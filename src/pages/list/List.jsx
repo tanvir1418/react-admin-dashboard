25ce2f70ce32070863e7ffbe8e43be5b0918c8c0
Tanvir <tanvirahmed1418@gmail.com>
2022-05-23 18:58:51 +0600

    Material UI Datatable Added

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Datatable from '../../components/datatable/Datatable';
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './list.scss';
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
 };
 
 export default List;
